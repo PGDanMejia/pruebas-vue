d6910f41b13d351a05486ba0c4fa6c06cbb1417d
PGDanMejia <39227781+PGDanMejia@users.noreply.github.com>
2019-12-26 09:25:56 -0600

    Initial commit

---
+++ ui/dependencies/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">TITULO DE PRUEBA</b-navbar-brand>
+    
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    
+        <b-collapse id="nav-collapse" is-nav>
+          
+    
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>Usuario</em>
+              </template>
+              <b-dropdown-item href="#">Perfil</b-dropdown-item>
+              <b-dropdown-item href="#">Cerrar sesión</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+</template>
---
+++ ui/dependencies/tabla-componente.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+    const myRequest = new Request('https://jsonplaceholder.typicode.com/users');
+    const myURL = myRequest.url; // http://localhost/api
+    const myMethod = myRequest.method; // GET
+    const myCred = myRequest.credentials; // omit
+    module.exports = {
+       mounted () {
+      fetch(myRequest)
+      .then(response => response.json())
+      .then(response => {
+        this.items = response;
+        console.log(response);
+        // ...
+      }).catch(error => {
+        console.error(error);
+      });
+    },
+    data() {
+      return {
+        fields: [
+          {
+            key: 'id',
+            sortable: true
+          },
+          {
+            key: 'name',
+            sortable: false
+          },
+          {
+            key: 'age',
+            label: 'age',
+            sortable: true,
+          },
+          {
+            key: 'email',
+            sortable: true,
+          }
+        ],
+        items: null
+      }
+    }
+  }
+</script>
