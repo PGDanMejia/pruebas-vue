17d2e74dc7fdcafc92b6c9b38aaef87566434a51
PGDanMejia <39227781+PGDanMejia@users.noreply.github.com>
2019-12-26 16:06:51 -0600

    new file

---
+++ ui/dependencies/reports.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+    const myRequest = new Request('https://jsonplaceholder.typicode.com/users');
+    const myURL = myRequest.url; // http://localhost/api
+    const myMethod = myRequest.method; // GET
+    const myCred = myRequest.credentials; // omit
+    module.exports = {
+       mounted () {
+      fetch(myRequest)
+      .then(response => response.json())
+      .then(response => {
+        this.items = response;
+        console.log(response);
+        // ...
+      }).catch(error => {
+        console.error(error);
+      });
+    },
+    data() {
+      return {
+        items: null
+      }
+    }
+  }
+</script>
