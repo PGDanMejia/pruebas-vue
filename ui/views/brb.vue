9a78d7ca8606dc51248af0dbb8e8cd43f4aa9add
sguzm <saul.guzman@agilesolutions.com>
2019-12-26 16:54:35 -0600

    view brb added

---
+++ ui/views/brb.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-container fluid>
+    <b-row class="nav-h15">
+      <b-col cols="12"></b-col>
+    </b-row>
+    <b-row class="nav-h85">
+      <b-col cols="4"></b-col>
+      <b-col cols="8"></b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: () => ({}),
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.nav-h15 {
+  height: 15vh;
+}
+.nav-h85 {
+  height: 85vh;
+}
+</style>
